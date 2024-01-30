20d6b3178513ee4c39aa31d8b944603ad24c80cc
SERGIO <150312598+Sergio9302@users.noreply.github.com>
2024-01-30 16:32:35 -0600

    Update hola.cpp
    agregar codigo

---
+++ hola.cpp
@@ -0,0 +1,27 @@
+/*           PRESENTACION
+NOMBRE:Sergio Alejandro Camarena Carbajal
+FECHA:29 de enero de 2024
+PROGRAMA: datos.cpp
+CENTRO UNIVERSITARIO DE LOS ALTOS/UNIVERSIDAD DE GUADALAJARA INGENIERIA EN COMPUTACION/2DO SEMESTRE
+PROFESOR:Sergio Franco Casillas
+DESCRIPCIION:Imprimir los datos personales
+*/
+
+
+
+
+
+#include <iostream>
+using namespace std;
+int main(){
+	printf("Sergio Alejandro Camarena Carbajal\n");		
+	printf("18 a�os\n");
+	printf("acatic\n");
+	printf("ingenieria en computacion\n");
+	printf("2do semestre\n");
+	
+	
+	
+	
+	return 0;
+}
